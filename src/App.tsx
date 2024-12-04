9adf25ecaefff985ef54df81482b2bb5235589ba
Alexandre Hannecart <111154530+Atuyto@users.noreply.github.com>
2024-12-04 13:10:03 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Alexandre-Hannecart>
         <div className="min-h-screen flex flex-col">
           <Navbar />
           <main className="flex-grow">
