7d8405dd1e27905788b33761347908d17ed21a1e
alexa <alexandresoune@gmail..com>
2024-12-06 20:46:07 +0100

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import react from "@vitejs/plugin-react-swc";
-import {defineConfig} from "vite";
-import * as path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
@@ -10,4 +10,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
