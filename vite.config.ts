d8d241f6c51ee5058228312f7be305cd5175f04a
Alexandre Hannecart <111154530+Atuyto@users.noreply.github.com>
2024-12-06 19:27:52 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "path"
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: "/Alexandre-Hannecart/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
