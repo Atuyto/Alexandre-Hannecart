5c14dc7da00345a26af8d8ab7fb723acfc706ef5
alexa <alexandresoune@gmail..com>
2024-12-06 17:44:42 +0100

    about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -10,7 +10,7 @@ const About = () => {
               <div>
                 <div className="aspect-square bg-secondary rounded-lg">
                   <img
-                      src="/Alexandre-Hannecart/moi.png"
+                      src="/moi.png"
                       alt="Project media"
                       className="max-w-full max-h-full object-contain transition-transform duration-300 group-hover:scale-110"
                   />
