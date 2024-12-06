728721be41cd5e5fce4ec0a13ed8a158fca547d2
alexa <alexandresoune@gmail..com>
2024-12-06 20:12:26 +0100

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import {defineConfig} from "vite";
-import * as path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: "/Alexandre-Hannecart/",
 });
